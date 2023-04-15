4e451e4677ca96b8883daf476f2696032436ef3b
David Russell <davidrussellrgb@gmail.com>
2023-04-15 13:24:42 -0500

    Fully functional decompression engine

---
+++ public/controller/decompressor.js
@@ -0,0 +1,101 @@
+import * as Utility from './utility.js';
+
+import { TreeNode } from '../model/treeNode.js';
+
+const WORD_LENGTH = 8;
+
+function setDecompressionStatus(state) {
+    document.getElementById('decompression-status').innerHTML = state;
+}
+
+async function reconstructTree(tree) {
+    setDecompressionStatus('Reconstructing tree');
+    let head = new TreeNode();
+    let currentNode = head;
+    for (let i = 0; i < tree.length; i++) {
+        while (!currentNode.isBridge) currentNode = currentNode.parent;
+        if (currentNode.leftChild == null) {
+            if (tree[i] === '0') {
+                currentNode.leftChild = new TreeNode({
+                    parent: currentNode,
+                });
+                currentNode = currentNode.leftChild;
+            }
+            else if (tree[i] === '1') currentNode.leftChild = new TreeNode({
+                character: tree[++i],
+                isBridge: false,
+                parent: currentNode,
+            });
+        }
+        else if (currentNode.righthild == null) {
+            if (tree[i] === '0') {
+                currentNode.rightChild = new TreeNode({
+                    parent: currentNode,
+                });
+                currentNode = currentNode.rightChild;
+            }
+            else if (tree[i] === '1') currentNode.rightChild = new TreeNode({
+                character: tree[++i],
+                isBridge: false,
+                parent: currentNode,
+            });
+            while (currentNode != null && currentNode.leftChild != null && currentNode.rightChild != null) currentNode = currentNode.parent;
+        }
+    }
+    return head;
+}
+
+async function reconstructText(bytes, head, numberOfBits) {
+    setDecompressionStatus('Reconstructing text');
+    let text = "";
+    let currentNode = head;
+
+    for (let i = 0; i < bytes.length; i++) {
+        let string = binaryStringFromNumber(bytes[i]);
+        while (string.length > 0) {
+            let bit = string[0];
+            string = string.substring(1);
+            if (numberOfBits > 0) {
+                if (bit === '0') currentNode = currentNode.leftChild;
+                else if (bit === '1') currentNode = currentNode.rightChild;
+                if (!currentNode.isBridge) {
+                    text += currentNode.character;
+                    currentNode = head;
+                }
+                numberOfBits--;
+            }
+        }
+    }
+    return text;
+}
+
+function binaryStringFromNumber(number) {
+    let string = number.toString(2);
+    while (string.length < WORD_LENGTH) string = `0${string}`;
+    return string;
+}
+
+export async function decompress(data) {
+    const tree = data.tree;
+    const head = await reconstructTree(tree);
+    Utility.sleep(250);
+    let numberOfBits = data.numberOfBits;
+    const bytes = data.bytes;
+    const text = await reconstructText(bytes, head, numberOfBits);
+    Utility.sleep(250);
+    return {
+        input: `${data.tree}\n\n${data.numberOfBits}\n\n${new TextDecoder().decode(data.bytes)}`,
+        text: text,
+        head: head,
+        bytes: bytes,
+    };
+}
+
+export async function createAndDownloadFile(data, filename) {
+    const link = document.createElement("a");
+    const file = new Blob([data.text], { type: 'text/plain' });
+    link.href = URL.createObjectURL(file);
+    link.download = filename;
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
--- public/viewpage/decompression.js
+++ public/viewpage/decompression.js
@@ -1,3 +1,6 @@
+import * as Decompressor from '../controller/decompressor.js';
+import * as Utility from '../controller/utility.js';
+
 import * as Element from './element.js';
 
 export function decompressionPage() {
@@ -6,12 +9,13 @@ export function decompressionPage() {
     let html;
 
     html = `
-        <div style="align-items: center; display:flex; flex-wrap:wrap; max-width:90vw; min-width:70vw; justify-content:center;">
+        <div style="align-items: center; display:flex; flex-wrap:wrap; height: 85vh; max-width:90vw; min-width:70vw; justify-content:center;">
             <div style="padding: 5vh 5vw;">
                 <h1>Decompression</h1>
                 <form id="form-decompress" method="post">
+                    <input id="file-upload" type="file" style="display: none" />
                     <div style="padding-bottom:5vh">
-                        <textarea id="textarea-input" name="input" placeholder="Upload a compressed file or enter text for decompression here" cols="50" rows="10" autofocus required></textarea>
+                        <textarea id="textarea-input" name="input" placeholder="Upload a compressed file for decompression" cols="50" rows="10" readonly></textarea>
                     </div>
                     <br>
                     <div style="display:flex; justify-content: space-around; width:100%;">
@@ -33,10 +37,112 @@ export function decompressionPage() {
 
     Element.root.innerHTML = html;
 
-    document.getElementById('form-decompress').addEventListener('submit', e => {
+    let tree, numberOfBits, bytes;
+
+    document.getElementById('file-upload').onchange = e => {
+        let file = e.target.files[0];
+        var reader = new FileReader();
+
+        reader.readAsArrayBuffer(file);
+        reader.onload = readerEvent => {
+            let text = new Uint8Array(readerEvent.target.result);
+            let string = "";
+            for (let i = 0; i < text.length; i++) {
+                if (tree === undefined || tree === null) {
+                    let currentChar = String.fromCharCode(text[i]);
+                    if (currentChar === '\n' && String.fromCharCode(text[i + 1]) === '\n'){
+                        tree = string;
+                        string = "";
+                        i++;
+                    }
+                    else string += String.fromCharCode(text[i]);
+                }
+                else if (numberOfBits === undefined || numberOfBits === null) {
+                    let currentChar = String.fromCharCode(text[i]);
+                    if (currentChar === '\n' && String.fromCharCode(text[i + 1]) === '\n'){
+                        numberOfBits = Number(string);
+                        i++;
+                    }
+                    else string += String.fromCharCode(text[i]);
+                }
+                else {
+                    bytes = text.subarray(i);
+                    break;
+                }
+            }
+            document.getElementById('textarea-input').value = new TextDecoder().decode(text);
+        }
+
+    };
+
+    document.getElementById('button-upload').addEventListener('click', function() {
+        document.getElementById('file-upload').click();
+    });
+
+    document.getElementById('form-decompress').addEventListener('submit', async e => {
         e.preventDefault();
-        const text = e.target.input.value;
-        // Show spinner
-        Element.root.innerHTML = `<div style="align-items:center; display:flex; justify-content:center; min-height: 65vh;"><div class="lds-dual-ring"></div></div>`
+        decompressionProgressPage();
+        const data = await Decompressor.decompress({
+            tree: tree,
+            numberOfBits: numberOfBits,
+            bytes: bytes,
+        });
+        postDecompressionPage(data);
+    });
+}
+
+function decompressionProgressPage() {
+    let html = `
+        <div style="align-items:center; display:flex; flex-direction:column; height:85vh; justify-content:center;">
+            <div class="row">
+                <h1 id="decompression-status"></h1>
+            </div>
+            <div class="row">
+                <div class="lds-dual-ring"></div>
+            </div>
+        </div>
+    `;
+    Element.root.innerHTML = html;
+}
+
+function postDecompressionPage(data) {
+    let html = `
+        <div style="align-items:center; display:flex; flex-wrap:wrap; height:85vh; justify-content:center; max-width:90vw; min-width:70vw;">
+            <div style="padding: 5vh 5vw">
+                <h1>Decompression complete!</h1>
+                <h2>
+                   ${Utility.percentage(data.input.length * 8, data.text.length * 8)} from ${Utility.fileSize(data.input.length * 8)} to ${Utility.fileSize(data.text.length * 8)}
+                </h2>
+                <div style="padding-bottom: 10px;">
+                    <hr class="rounded">
+                </div>
+                <form id="form-download-decompressed-file" method="get">
+                    <div style="align-items:center; display:flex; flex-wrap:wrap; gap:5vw; justify-content:space-around; width:100%;">
+                        <div style="display:flex; flex-direction:column; gap:10px;">
+                            <input type="text" name="filename" placeholder="Filename for download" style="height:100%; padding:5px;">
+                            <div id="filename-error" style="display:none"></div>
+                        </div>
+                        <button type="submit">Download</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    `;
+
+    Element.root.innerHTML = html;
+    document.getElementById('form-download-decompressed-file').addEventListener('submit', async e => {
+        e.preventDefault();
+        const filename = e.target.filename.value;
+        let error = Utility.validateFilename(filename);
+        console.log(error);
+        if (error != null) {
+            let filenameErrorElement = document.getElementById('filename-error');
+            filenameErrorElement.style = 'color:red; display:inline; font-weight:bold;';
+            filenameErrorElement.innerHTML = error;
+        }
+        else {
+            document.getElementById('filename-error').style = 'display:none';
+            await Decompressor.createAndDownloadFile(data, filename);
+        }
     });
 }
