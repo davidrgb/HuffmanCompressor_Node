dcb4ecb9f7d3429b1ed421b3ea2ff583398390fb
David Russell <davidrussellrgb@gmail.com>
2023-06-14 01:46:23 -0500

    Fixed Express serve path

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 app.get('*', function(req, res) {
-    res.sendFile(path.resolve('public/index.html'));
+    res.sendFile(path.resolve('../public/index.html'));
 });
 
 app.listen(3000, () =>
